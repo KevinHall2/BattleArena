55d7782df27255fbf467c240dc3a416400477845
KevinHall2 <khjiitech@gmail.com>
2024-09-23 16:31:21 -0500

    Set up the BattleArena framework with the Game and Character classes

--- Character.cs
+++ Character.cs
@@ -8,5 +8,82 @@ namespace BattleArena
 {
     internal class Character
     {
+        private string _name = "Character";
+
+        private float _maxHealth = 10;
+
+        private float _health = 10;
+
+        private float _attackPower = 1;
+
+        private float _defensePower = 1;
+
+
+        public string Name
+        {
+            get => _name;
+        }
+
+        public float MaxHealth
+        {
+            get => _maxHealth;
+        }
+
+        public float Health
+        {
+            get => _health;
+            private set
+            {
+                _health = Math.Clamp(value, 0, _maxHealth);
+            }
+        }
+
+        public float AttackPower
+        {
+            get => _attackPower;
+        }
+
+        public float DefensePower
+        {
+            get => _defensePower;
+        }
+
+        public Character(string name,
+            float maxHealth,
+            float attackPower,
+            float defensePower)
+        {
+            _name = name;
+            _maxHealth = maxHealth;
+            _health = maxHealth;
+            _attackPower = attackPower;
+            _defensePower = defensePower;
+        }
+
+        public float Attack(Character other)
+        {
+            float damage = MathF.Max(0, _attackPower - other.DefensePower);
+            other.TakeDamage(damage);
+            return damage;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            _health -= damage;
+            if (Health == 0)
+            {
+                Die();
+            }
+        }
+
+        public void Heal(float health)
+        {
+            Health += health;
+        }
+
+        private void Die()
+        {
+            Console.WriteLine(Name + "has been slain.");
+        }
     }
 }
--- Game.cs
+++ Game.cs
@@ -8,5 +8,71 @@ namespace BattleArena
 {
     internal class Game
     {
+        private bool _gameOver = false;
+
+
+
+        private int GetInput(string description, string option1, string option2)
+        {
+            ConsoleKeyInfo key;
+
+            int inputRecieved = 0;
+
+            while (inputRecieved != 1 && inputRecieved != 2)
+            {
+                //prints out the two options, asks for player input, increments inputRecieved depending on the choice,
+                //or loops this while statement again if the input does not match one of the options
+                Console.Clear();
+                Console.WriteLine(description);
+                Console.WriteLine("1: ");
+                Console.WriteLine("2: ");
+                Console.Write(">");
+
+                key = Console.ReadKey();
+
+                if (key.KeyChar == '1')
+                {
+                    inputRecieved = 1;
+                }
+                else if (key.KeyChar == '2')
+                {
+                    inputRecieved = 2;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input.");
+                    Console.ReadKey();
+                }
+
+            }
+            Console.WriteLine();
+            return inputRecieved;
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void End()
+        {
+
+        }
+
+
+        public void Run()
+        {
+            Start();
+            while (!_gameOver)
+            {
+                Update();
+            }
+            End();
+        }
     }
 }
